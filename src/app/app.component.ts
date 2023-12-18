0920eadba773c4657fc38dc5174ccfc8cdb5d58d
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2023-12-18 17:07:05 -0300

    local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'mfe-1';
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+    this.obterDoLocalStorage()
+    
+  }
+  
+  obterDoLocalStorage(): void {
+    
+    const suaVariavel = localStorage.getItem('suaChave');
+    console.log('Valor recuperado do localStorage:', suaVariavel);
+  }
+ 
 }
